3ff961609d304ffc5f07ac4331201af1a698fb6c
Guannan-Shen <guannan.shen.ai@gmail.com>
2019-03-24 01:05:23 -0600

    global DE analysis

--- Code/Antibody_Vgenes/lm_assumption_DEofV.R
+++ Code/Antibody_Vgenes/lm_assumption_DEofV.R
@@ -2,6 +2,7 @@
 library(edgeR)
 library(tidyverse)
 library(readxl)
+library(openxlsx)
 ############### rnaseq
 setwd("~/Documents/gitlab/Cario_RNASeq_Microbiom_Inte/DataRaw/Vgenes/")
 cnts_raw <- read_excel("HIV_Infected_vs_Uninfected_relaxed_mapping_geneCounts.xlsx")
@@ -10,21 +11,23 @@ head(cnts_raw)
 vgenes <- read_excel("/home/guanshim/Documents/gitlab/Cario_RNASeq_Microbiom_Inte/DataProcessed/Vgenes/Vgenes_list.xlsx")
 head(vgenes)
 ######clinical and microbiome data #########
-clin_micro <- read_excel("/home/guanshim/Documents/gitlab/Cario_RNASeq_Microbiom_Inte/DataProcessed/Vgenes/clinical_Microbiome_pre.xlsx")
+clin_micro <- read_excel("/home/guanshim/Documents/gitlab/Cario_RNASeq_Microbiom_Inte/DataProcessed/Vgenes/clin_microbiome.xlsx")
 head(clin_micro)
-sample_size <- length(clin_micro$pid)
+sample_size <- length(clin_micro$ID )
 sample_size
 ## test the ids between rna-seq and clin microbiome data
-sum( colnames(cnts_raw)[-c(1:3)] != clin_micro$pid )
+# sum( colnames(cnts_raw)[-c(1:3)] != clin_micro$pid )
 colnames(cnts_raw)[-c(1:3)]
 ## get cnts for norm and DE 
 ## filter to guarantee cnts larger than 1 
 cnts_fsym <- cnts_raw[rowSums(as.matrix(cnts_raw[, -c(1:3)]) )>=(3*ncol(cnts_raw[, 1:32])), ]
 ## processing of rnaseq data
-cnts <- cnts_fsym %>% dplyr::select(-c(Symbol, Length ) ) %>% tibble::column_to_rownames("Gene_ID")
+######################## targeted v genes of y ########################
+cnts <- cnts_fsym %>% filter(Symbol %in% vgenes$Vsymbols ) %>%
+   dplyr::select(-c(Gene_ID, Length ) ) %>% tibble::column_to_rownames("Symbol")
 cnts <- as.matrix(cnts)
 dim(cnts)
-######## edgeR TMM ###############
+######## edgeR TMM Normalization ###############
 ## edgeR object
 group <- c(rep("Control", 13), rep("HIV", 19))
 y <- DGEList(counts= cnts, group=group)
@@ -34,16 +37,91 @@ y$samples
 #  TMM counts 
 # If you run the cpm function on a DGEList object which contains TMM normalisation factors 
 # then you will get TMM normalised counts. 
-# Here is a snippet of the source code for the cpm function:
+# Here is a snippet of the source code for the cpm function
 cnts.edger <- edgeR::cpm(y)
-dim(cnts.edger) == dim(cnts)
-# as data frame
+nrow(cnts.edger) == 45
+## 
 
-cnts_fsym <- data.frame(cnts_fsym)
-cnts.edger <- data.frame(cnts.edger)
-cnts.edger$Symbol <- cnts_fsym$Symbol
-cnts.edger[]
-# adjusting for age gender
+# write.xlsx(cnts.edger ,
+#            "~/Documents/gitlab/Cario_RNASeq_Microbiom_Inte/DataProcessed/Vgenes/Data_generated/vgenes_tmm.xlsx",
+#            sheetName="rlog counts_correlation test")
+#################### adjusting for age gender DE analysis ################### 
+# get the design matrix
+# the group
+gender <- clin_micro$Gender
+group 
+age <- clin_micro$Age
+design <- model.matrix(~ age + gender + group)
+design
+# general glm 
+y <- estimateDisp(y, design, robust=TRUE)
+fit <- glmFit(y, design)
+lrt <- glmLRT(fit)
+topTags(lrt)
+##################### global norm then filtered DE #######################
+# the same design, different y
+cnts <- cnts_fsym %>% dplyr::select(-c(Symbol, Length ) ) %>% tibble::column_to_rownames("Gene_ID")
+cnts <- as.matrix(cnts)
+dim(cnts)
+##### 
+y <- DGEList(counts= cnts, group=group)
+## normalization 
+y <- calcNormFactors(y, method = "TMM")
+y$samples
 
+###
+head(y$counts )
+sum( rownames(y$counts) !=  cnts_fsym$Gene_ID )
+norm_filtered <- data.frame(edgeR::cpm(y))
+norm_filtered$Symbol <- cnts_fsym$Symbol
+norm_v <- norm_filtered %>% filter(Symbol %in% vgenes$Vsymbols ) %>% tibble::column_to_rownames("Symbol")
+dim(norm_v)
+head(norm_v)
+norm_v <- as.matrix(norm_v)
 
+##### 
+y <- DGEList(counts= norm_v, group=group)
+y <- estimateDisp(y, design, robust=TRUE)
+fit <- glmFit(y, design)
+lrt <- glmLRT(fit)
+topTags(lrt)
+
+########################### whole global ###############################
+##################### global norm then filtered DE #######################
+# the same design, different y
+cnts <- cnts_fsym %>% dplyr::select(-c(Symbol, Length ) ) %>% tibble::column_to_rownames("Gene_ID")
+cnts <- as.matrix(cnts)
+dim(cnts)
+##### 
+y <- DGEList(counts= cnts, group=group)
+## normalization 
+y <- calcNormFactors(y, method = "TMM")
+y$samples
+## get the counts 
+cnts.edger <- edgeR::cpm(y, normalized.lib.sizes = TRUE,
+                         log = FALSE)
+dim(cnts.edger)
+sum(rownames(cnts.edger) != cnts_fsym$Gene_ID)
+cnts.edger.vgenes <- data.frame(cnts.edger) %>% mutate(Symbol = cnts_fsym$Symbol) %>%
+                     filter(Symbol %in% vgenes$Vsymbols) %>% select(Symbol, everything())
+dim(cnts.edger.vgenes)
+tail(cnts.edger.vgenes)
+
+write.xlsx(cnts.edger.vgenes ,
+           "~/Documents/gitlab/Cario_RNASeq_Microbiom_Inte/DataProcessed/Vgenes/Data_generated/vgenes_tmm.xlsx",
+           sheetName="TMM counts, unlogged")
+
+y <- estimateDisp(y, design, robust=TRUE)
+fit <- glmFit(y, design)
+lrt <- glmLRT(fit)
+sum( rownames(lrt$table) != cnts_fsym$Gene_ID )
+lrt_v <- data.frame(lrt$table) %>% mutate(Symbol = cnts_fsym$Symbol ,
+                                          FDR = p.adjust(PValue, method = "BH" ) ) %>% 
+                                     filter( Symbol %in% vgenes$Vsymbols ) %>% 
+                                    select( Symbol, everything()) %>% arrange(FDR)
+
+write.xlsx(lrt_v ,
+          "~/Documents/gitlab/Cario_RNASeq_Microbiom_Inte/DataProcessed/Vgenes/Data_generated/DE_vgenes_hiv.xlsx",
+           sheetName="Global_DE_age_gender_hiv_Vgenes")
 ########################### check the lm assumption of single mutation  ############################
+
